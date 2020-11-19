e797b0053a87485a9fd436eaa9cfade7202ca3b3
KarmaKamikaze <nicolaijgn@gmail.com>
2020-11-19 13:13:11 +0100

    Employee struct and titles enum

--- replacer.c
+++ replacer.c
@@ -1,8 +1,42 @@
 #include <stdio.h>
 
-int main(int argc, char const *argv[])
-{
+#define MAX_POSITIONS 20
 
-    printf("Hello world.");
-    return 0;
+typedef enum titles {
+  closing_responsible,
+  employee,
+  head_manager,
+  manager,
+  opening_responsible,
+  shift_supervisor
+} titles;
+
+typedef struct employee_s {
+  int youth_worker; /* Boolean */
+  int number_of_positions;
+  int positions[MAX_POSITIONS];
+} employee_s;
+
+void print_employee(employee_s employee);
+
+int main(int argc, char const *argv[]) {
+  employee_s employee;
+  employee.youth_worker = 1;
+  employee.number_of_positions = 1;
+  employee.positions[0] = manager;
+  employee.positions[1] = shift_supervisor;
+
+  print_employee(employee);
+
+  return 0;
+}
+
+void print_employee(employee_s employee) {
+  int i;
+  int size = employee.number_of_positions;
+
+  printf("Under 18? %d\nTitles:", employee.youth_worker);
+  for (i = 0; i <= size; i++) {
+    printf(" %d\n", employee.positions[i]);
+  }
 }
