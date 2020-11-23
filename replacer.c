f70cb693ac3fa4866cdae8d1af5f5488d783e80d
Lasse <lass5588@icloud.com>
2020-11-23 15:55:10 +0100

    count fields

--- replacer.c
+++ replacer.c
@@ -1,58 +1,94 @@
-#include <json-c/json.h>
 #include <stdio.h>
+#include <string.h>
 
-#define MAX_POSITIONS 20
-#define MAX_STRING_LENGTH 1000
+#define MAX_POSITIONS 100
+#define MAX_STRING_LENGTH 100
 
-typedef enum titles {
+/* typedef enum titles {
   closing_responsible,
   employee,
   head_manager,
   manager,
   opening_responsible,
   shift_supervisor
-} titles;
+} titles; */
 
 typedef struct employee_s {
   int youth_worker;        /* Boolean */
   int number_of_positions; /* Used internally */
-  int positions[MAX_POSITIONS];
+  char positions[MAX_STRING_LENGTH][MAX_POSITIONS];
 } employee_s;
 
-void print_employee(employee_s employee);
+void print_employee(employee_s employee[]);
+void parse_employee_data(employee_s employee[]);
+int count_elements(FILE *fp);
 
 int main(int argc, char const *argv[]) {
-  employee_s employee;
-  employee.youth_worker = 1;
-  employee.number_of_positions = 1;
-  employee.positions[0] = manager;
-  employee.positions[1] = shift_supervisor;
+  employee_s employee[MAX_POSITIONS];
+  /* employee[0].youth_worker = 1;
+  employee[0].number_of_positions = 1;
+  strcpy(employee[0].positions[0][0], "manager");
+  strcpy(employee[0].positions[1][0], "shift_supervisor"); */
 
-  print_employee(employee);
+  /* print_employee(employee); */
+
+  parse_employee_data(employee);
 
   return 0;
 }
 
-void parse_employee_data(employee_s employee) {
+void parse_employee_data(employee_s employee[]) {
   FILE *fp;
-  char input_file_data[MAX_STRING_LENGTH];
+  int i; /* Counter */
 
-  fp = fopen("employees.json", "r");
-  if (fp == NULL)
-    printf("Cannot open employee.json data file.\n");
+  /* Creates af file if it doesn't exist.*/
+  do
+  {
+    fp = fopen("employee.txt", "r");
+    if (fp == NULL)
+    {
+      printf("file employee.txt was not found, creating new file.");
+      fp = fopen("employee.txt", "w");
+      fclose(fp);
+    }
+  } while (fp == NULL);
 
-  fscanf(fp, "%s", input_file_data);
+  printf(" elements counted %d \n", count_elements(fp));
+
+  for (i = 0; i <= count_elements(fp); i++)
+  {
+
+  }
+  /* fscanf(fp, "%s, %s, %d"); */
 
   fclose(fp);
 }
 
-void print_employee(employee_s employee) {
-  int i;
-  int size = employee.number_of_positions;
 
-  printf("Under 18? %s\nTitles:", employee.youth_worker ? "Yes." : "No.");
+
+/*  */ 
+int count_elements(FILE *fp)
+{
+  int elements = 0, ch = 0;
+  fseek(fp, 0, SEEK_SET); /* Makes sure we ALWAYS reed from the start of our file.*/
+  while (ch != EOF)
+  {
+    ch = fgetc(fp);
+    if (ch == ',' || ch == '\n')
+    {
+      elements++;
+    }
+  }
+  return elements;
+}
+
+void print_employee(employee_s employee[]) {
+  int i;
+  int size = employee[0].number_of_positions;
+
+  printf("Under 18? %s\nTitles:", employee[0].youth_worker ? "Yes." : "No.");
   for (i = 0; i <= size; i++) {
-    printf(" %d", employee.positions[i]);
+    /* printf(" %s", employee[i].positions[0]); */
   }
   printf("\n");
 }
