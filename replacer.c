7cf6589634c921d9f60328548c57d1ee232732e7
Sarmisuper <sarmilan3@gmail.com>
2020-11-30 17:45:17 +0100

    Add_new_employee funktionen er blevet gennemarbejdet efter reviews og sættes til genreview

--- replacer.c
+++ replacer.c
@@ -22,81 +22,63 @@ typedef struct employee_s {
   int positions[TOTAL_POSITIONS];
 } employee_s;
 
-/*void print_employee(employee_s employee);*/
 void add_new_employee(employee_s employees[], int *num_of_employees);
-void scan_name(employee_s employees[], int current_employee, char *throwaway_string);
-void scan_phone_number(employee_s employees[], int current_employee, char *throwaway_string);
-void scan_youth_worker_or_availability(employee_s employees[], int current_employee, char *throwaway_string, char *string_youth_or_availability);
-void scan_number_of_positions(employee_s employees[], int current_employee, char *throwaway_string);
-void scan_positions(employee_s employees[], int current_employee, char *throwaway_string , char positions_str_arr[TOTAL_POSITIONS][20]);
+void scan_name(employee_s employees[], int current_employee);
+void scan_phone_number(employee_s employees[], int current_employee);
+void scan_youth_worker_or_availability(employee_s employees[], int current_employee, char *string_youth_or_availability);
+void scan_number_of_positions(employee_s employees[], int current_employee);
+void scan_positions(employee_s employees[], int current_employee, char positions_str_arr[TOTAL_POSITIONS][20]);
 void print_employee_after_adding(employee_s employees[], int current_employee, char positions_str_arr[TOTAL_POSITIONS][20]);
 
 int main(int argc, char const *argv[]) {
   employee_s employees[MAX_EMPLOYEES];
   int num_of_employees = 10; /*Denne variable skal bestemmes ud fra csv filen. Værdien er bare en placeholder pt.*/
-  employees[0].youth_worker = 1;
-  employees[0].number_of_positions = 2;
-  employees[0].positions[0] = manager;
-  employees[0].positions[1] = shift_supervisor;
-
-
-  /*print_employee(employees[0]);*/
-
+  
   add_new_employee(employees, &num_of_employees);
 
   return 0;
 }
 
-/*
-void print_employee(employee_s employee) {
-  int i;
-  int size = employee.number_of_positions;
-
-  printf("Under 18? %d\nTitles:", employee.youth_worker);
-  for (i = 0; i <= size; i++) {
-    printf(" %d\n", employee.positions[i]);
-  }
-}
-*/
-
-/*Denne funktion tilføjer en ny employee til arrayet employees[]. */
+/*This function adds a new employee to the employees array.*/
 void add_new_employee(employee_s employees[], int *num_of_employees) {
   char positions_str_arr[TOTAL_POSITIONS][20] = {"pos0", "pos1", "pos2", "pos3", "pos4", "pos5"};
-  char throwaway_string[10]; /*throwaway_string is used to prevent scanf-overflow*/
   
   
-  scan_name(employees, *num_of_employees+1, throwaway_string);
+  /*Note that the index of the employees array is 1 smaller that the num_of_employees, which means that the 10th employee has the index 9. 
+   *Therefore the new employee (employee 11) has the index equal to num_of_employees (10)*/
+  scan_name(employees, *num_of_employees);
 
-  scan_phone_number(employees, *num_of_employees+1, throwaway_string);
+  scan_phone_number(employees, *num_of_employees);
 
-  scan_youth_worker_or_availability(employees, *num_of_employees+1, throwaway_string, "youth");
+  scan_youth_worker_or_availability(employees, *num_of_employees, "youth");
   
-  scan_youth_worker_or_availability(employees, *num_of_employees+1, throwaway_string, "availability");
+  scan_youth_worker_or_availability(employees, *num_of_employees, "availability");
   
-  scan_number_of_positions(employees, *num_of_employees+1, throwaway_string);
+  scan_number_of_positions(employees, *num_of_employees);
   
-  scan_positions(employees, *num_of_employees+1, throwaway_string, positions_str_arr);
+  scan_positions(employees, *num_of_employees, positions_str_arr);
   
-  print_employee_after_adding(employees, *num_of_employees+1, positions_str_arr);
+  print_employee_after_adding(employees, *num_of_employees, positions_str_arr);
   
-   
-
 
  /*Slet employee funktionen kan bruges her til at slette hvis man ikke er tilfreds med resultatet*/
+
   *num_of_employees++;
 }
 
 
 
 /*This function scans after a name*/
-void scan_name(employee_s employees[], int current_employee, char *throwaway_string){
+void scan_name(employee_s employees[], int current_employee){
+  char throwaway_string[10]; /*throwaway_string is used to prevent scanf-overflow*/
   printf("FULL NAME: ");
   scanf(" %[a-zA-Z ]", employees[current_employee].name);
   gets(throwaway_string);
 }
 
 /*This function scans after a phone number. If the number is too lang or too short the do-while loop repeats*/
-void scan_phone_number(employee_s employees[], int current_employee, char *throwaway_string){
+void scan_phone_number(employee_s employees[], int current_employee){
+  char throwaway_string[10];
   do {
     printf("PHONE NUMBER: ");
     scanf(" %[0-9]", employees[current_employee].phone_number);
@@ -109,8 +91,8 @@ void scan_phone_number(employee_s employees[], int current_employee, char *throw
 /*This function asks a yes/no question and only accepts words that start with y or n. Case insensitive.
  *After the loops, the answer is converted into a boolean integer.
  *A string is inserted when the function is called and this is used to determine which question is asked*/
-void scan_youth_worker_or_availability(employee_s employees[], int current_employee, char *throwaway_string, char *string_youth_or_availability){
-  char temp_string[10];
+void scan_youth_worker_or_availability(employee_s employees[], int current_employee, char *string_youth_or_availability){
+  char temp_string[10], throwaway_string[10];
   do {
     printf("%s (YES/NO) ", !strcmp(string_youth_or_availability, "youth") ? "IS EMPLOYEE A YOUTH WORKER?" : "IS EMPLOYEE AVAILABLE ON WEEKDAYS FROM 8-16?");
     scanf(" %s", temp_string);
@@ -136,7 +118,8 @@ void scan_youth_worker_or_availability(employee_s employees[], int current_emplo
 
 
 /*This function scans after the number of positions the employee has and only accepts values larger than 0 and less than TOTAL_POSITIONS*/
-void scan_number_of_positions(employee_s employees[], int current_employee, char *throwaway_string){
+void scan_number_of_positions(employee_s employees[], int current_employee){
+  char throwaway_string[10];
   do {
     printf("NUMBER OF POSITIONS: ");
     scanf(" %d", &employees[current_employee].number_of_positions);
@@ -150,8 +133,9 @@ void scan_number_of_positions(employee_s employees[], int current_employee, char
 /*This function prints the choosable positions and scans after them. The positions and their corresponding integers are printed.
  *Afterwards it scans for the integers in a for-loop, performing scanf overflow. As soon as an integer is too small or too large, 
  *the for loop breaks and the while loop starts over*/
-void scan_positions(employee_s employees[], int current_employee, char *throwaway_string , char positions_str_arr[TOTAL_POSITIONS][20]){
+void scan_positions(employee_s employees[], int current_employee, char positions_str_arr[TOTAL_POSITIONS][20]){
   int i, j, has_duplicates_bool, number_of_scanned_numbers;
+  char throwaway_string[10];
 
   do {
     printf("ENTER DIGITS FOR THE CORRESPONDING POSITIONS: (FORMAT: x,y,z)\n");
@@ -188,7 +172,7 @@ void scan_positions(employee_s employees[], int current_employee, char *throwawa
     }
 
   } while (employees[current_employee].positions[i-1] < 0 || employees[current_employee].positions[i-1] > TOTAL_POSITIONS || number_of_scanned_numbers != employees[current_employee].number_of_positions ||has_duplicates_bool); 
-  /*Her trækkes 1 fra i, siden for loopet incrementer inden den breakes hvilket betyder at i er for høj*/
+  /*Here 1 is subtracted from i, since the for-loop increments i before it breaks, which means i is too large*/
 }
 
 
